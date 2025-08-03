ce578cb8a721a6d9341ec7cec0a0eb056fd471ff
Anton <termythter@gmail.com>
2025-08-03 23:01:47 +0300

    StringHelper added

---
+++ Porter/Helpers/StringHelper.cs
@@ -0,0 +1,17 @@
+﻿using System.Linq;
+
+namespace Porter.Helpers
+{
+	public static class StringHelper
+	{
+		public static bool IsAnyNullOrEmpty(params string?[] strings)
+		{
+			return strings.Any(string.IsNullOrEmpty);
+		}
+
+		public static bool IsAllNullOrEmpty(params string?[] strings)
+		{
+			return strings.All(string.IsNullOrEmpty);
+		}
+	}
+}
