efb20b5a7bcceaf3bc933bd6ac50d5bd5483dde8
Anton <termythter@gmail.com>
2025-08-08 00:01:30 +0300

    added SshConnection and PortForwardManager

---
+++ Porter/Services/PortForwardManager.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+using Porter.Models;
+
+namespace Porter.Services
+{
+	public class PortForwardManager : IDisposable
+	{
+		private readonly SemaphoreSlim _semaphore = new(1, 1);
+
+		private readonly List<SshConnection> _connections = [];
+
+		public async Task<bool> StartForward(
+			SshTunnel tunnel,
+			Action<Exception>?
+			exceptionCallback = null,
+			Func<PrivateKey, Task<string?>>? promptPassphrase = null,
+			CancellationToken? cancellationToken = null)
+		{
+			try
+			{
+				// initiate async immediately
+				return await Task.Run(async () => await StartForwardInternal(tunnel, exceptionCallback, promptPassphrase, cancellationToken));
+			}
+			catch
+			{
+				return false;
+			}
+		}
+
+		public void StopForward(SshTunnel tunnel)
+		{
+			if (GetConnectionByTunnelId(tunnel.Id) is { } connection && connection.IsForwardStarted(tunnel.Id))
+			{
+				connection.StopForward(tunnel.Id);
+
+				//if(connection.Tunnels.Count == 0)
+				//{
+				//	_connections.Remove(connection);
+				//	connection.Dispose();
+				//}
+			}
+		}
+
+		public bool IsForwardStarted(SshTunnel tunnel)
+		{
+			return GetConnectionByTunnelId(tunnel.Id) is { } connection && connection.IsForwardStarted(tunnel.Id);
+		}
+
+		public void Dispose()
+		{
+			foreach (var connection in _connections)
+			{
+				connection.Dispose();
+			}
+
+			_connections.Clear();
+		}
+
+		private async Task<bool> StartForwardInternal(
+			SshTunnel tunnel,
+			Action<Exception>? exceptionCallback = null,
+			Func<PrivateKey, Task<string?>>? promptPassphrase = null,
+			CancellationToken? cancellationToken = null)
+		{
+			await _semaphore.WaitAsync(cancellationToken ?? CancellationToken.None);
+
+			try
+			{
+				if (GetMatchedConnectionForTunnel(tunnel) is not { } connection)
+				{
+					if (tunnel.SshServer is not null && tunnel.PrivateKey is not null)
+					{
+						connection = new SshConnection(tunnel.SshServer!, tunnel.PrivateKey!);
+						_connections.Add(connection);
+					}
+					else
+					{
+						return false;
+					}
+				}
+
+				if (!connection.IsConnected &&
+					!await connection.ConnectAsync(promptPassphrase, cancellationToken))
+				{
+					return false;
+				}
+
+				if (!connection.IsForwardStarted(tunnel.Id))
+				{
+					connection.StartForward(tunnel, exceptionCallback);
+				}
+			}
+			finally
+			{
+				_semaphore.Release();
+			}
+
+			return true;
+		}
+
+		private SshConnection? GetMatchedConnectionForTunnel(SshTunnel tunnel)
+		{
+			return _connections.FirstOrDefault(c => c.IsConnectionMatched(tunnel));
+		}
+
+		private SshConnection? GetConnectionByTunnelId(Guid tunnelId)
+		{
+			return _connections.FirstOrDefault(c => c.Tunnels.Contains(tunnelId));
+		}
+	}
+}
---
+++ Porter/Services/SshConnection.cs
@@ -0,0 +1,192 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+using Porter.Models;
+
+using Renci.SshNet;
+using Renci.SshNet.Common;
+
+namespace Porter.Services
+{
+	public class SshConnection : IDisposable
+	{
+		private const string LOCALHOST = "127.0.0.1";
+
+		private SshClient? _sshClient;
+
+		private readonly ConcurrentDictionary<Guid, ForwardedPortLocal> _forwards = new();
+
+		public ICollection<Guid> Tunnels => _forwards.Keys;
+
+		public SshServer SshServer { get; }
+
+		public PrivateKey PrivateKey { get; }
+
+		public bool IsConnected => _sshClient?.IsConnected ?? false;
+
+		public SshConnection(SshServer sshServer, PrivateKey privateKey)
+		{
+			SshServer = new SshServer
+			{
+				Name = sshServer.Name,
+				User = sshServer.User,
+				Host = sshServer.Host,
+				Port = sshServer.Port,
+			};
+
+			PrivateKey = new PrivateKey(privateKey.FilePath)
+			{
+				Name = privateKey.Name,
+			};
+		}
+
+		public async Task<bool> ConnectAsync(Func<PrivateKey, Task<string?>>? promptPassphrase = null, CancellationToken? cancellationToken = null)
+		{
+			if (_sshClient?.IsConnected == true || cancellationToken?.IsCancellationRequested == true)
+				return _sshClient?.IsConnected ?? false;
+
+			if (_sshClient is not null)
+			{
+				await _sshClient.ConnectAsync(cancellationToken ?? CancellationToken.None);
+				return true;
+			}
+
+			if (await GetPrivateKeyFile(PrivateKey, promptPassphrase) is not { } keyFile)
+				return false;
+
+			var auth = new PrivateKeyAuthenticationMethod(SshServer.User, keyFile);
+			var connectionInfo = new ConnectionInfo(SshServer.Host, (int)SshServer.Port!, SshServer.User, auth);
+
+			_sshClient = new SshClient(connectionInfo);
+
+			await _sshClient.ConnectAsync(cancellationToken ?? CancellationToken.None);
+			return true;
+		}
+
+		public bool IsConnectionMatched(SshTunnel tunnel)
+		{
+			return SshServer.Equals(tunnel.SshServer) && PrivateKey.Equals(tunnel.PrivateKey);
+		}
+
+		public void StartForward(SshTunnel tunnel, Action<Exception>? exceptionCallback = null)
+		{
+			var forward = AddOrGetLocalForward(tunnel, exceptionCallback);
+			forward?.Start();
+		}
+
+		public void StopForward(Guid tunnelId)
+		{
+			StopForwardByTunnelId(tunnelId);
+		}
+
+		public bool IsForwardStarted(Guid tunnelId)
+		{
+			return _forwards.TryGetValue(tunnelId, out var forward) && forward.IsStarted;
+		}
+
+		public void Dispose()
+		{
+			foreach (var entry in _forwards.Values)
+			{
+				entry.Dispose();
+			}
+
+			_forwards.Clear();
+
+			if (_sshClient?.IsConnected ?? false)
+			{
+				_sshClient.Disconnect();
+			}
+
+			_sshClient?.Dispose();
+		}
+
+		private static async Task<PrivateKeyFile?> GetPrivateKeyFile(PrivateKey privateKey, Func<PrivateKey, Task<string?>>? promptPassphrase)
+		{
+			var passphrase = default(string);
+
+			try
+			{
+				do
+				{
+					try
+					{
+						return passphrase is null
+							? new PrivateKeyFile(privateKey.FilePath)
+							: new PrivateKeyFile(privateKey.FilePath, passphrase);
+
+					}
+					catch (SshException ex) when (ex is SshPassPhraseNullOrEmptyException ||
+												  // handling Renci.SshNet.Common.SshException: 'MAC verification failed for PuTTY key file'
+												  ex.Message.Contains("putty", StringComparison.OrdinalIgnoreCase))
+					{
+						if (promptPassphrase is null)
+							break;
+
+						passphrase = await promptPassphrase(privateKey);
+					}
+				}
+				while (passphrase is not null);
+			}
+			catch
+			{
+				return null;
+			}
+
+			return null;
+		}
+
+		private ForwardedPortLocal? AddOrGetLocalForward(SshTunnel tunnel, Action<Exception>? exceptionCallback = null)
+		{
+			if (_sshClient is null)
+				return null;
+
+			if (!_forwards.TryGetValue(tunnel.Id, out var forward))
+			{
+				forward = AddLocalForward(tunnel, exceptionCallback);
+			}
+
+			return forward;
+		}
+
+		private ForwardedPortLocal? AddLocalForward(SshTunnel tunnel, Action<Exception>? exceptionCallback = null)
+		{
+			if (_sshClient is null)
+				return null;
+
+			var forward = new ForwardedPortLocal(LOCALHOST, (uint)tunnel.LocalPort!, tunnel.RemoteServer!.Host, (uint)tunnel.RemoteServer.Port!);
+			forward.Exception += (_, e) =>
+			{
+				StopForwardByTunnelId(tunnel.Id);
+				exceptionCallback?.Invoke(e.Exception);
+			};
+
+
+			_sshClient.AddForwardedPort(forward);
+			Debug.WriteLine($"Forwarding added to _sshClient, _sshClient.ForwardedPorts.Count = {_sshClient.ForwardedPorts.Count()}");
+			_forwards[tunnel.Id] = forward;
+			Debug.WriteLine($"Forwarding added to _forwards, _forwards.Count = {_forwards.Count}");
+			return forward;
+		}
+
+		private void StopForwardByTunnelId(Guid tunnelId)
+		{
+			if (_forwards.TryRemove(tunnelId, out var forward))
+			{
+				forward.Stop();
+
+				_sshClient?.RemoveForwardedPort(forward);
+
+				Debug.WriteLine($"Forwarding removed from _sshClient and _forwards;" +
+					$"\n_sshClient.ForwardedPorts.Count = {_sshClient?.ForwardedPorts.Count()}" +
+					$"\n_forwards.Count = {_forwards.Count}");
+			}
+		}
+	}
+}
