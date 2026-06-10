f6f5311a4c863eb862e8d034ccd08f7f49670b1f
Anton <termythter@gmail.com>
2026-06-10 20:19:03 +0300

    added Description attribute to AppTheme enum

--- Porter/Enums/AppTheme.cs
+++ Porter/Enums/AppTheme.cs
@@ -1,8 +1,15 @@
-﻿namespace Porter.Enums;
+﻿using System.ComponentModel;
+
+namespace Porter.Enums;
 
 public enum AppTheme
 {
+	[Description("System Default")]
 	SystemDefault = 0,
+
+	[Description("Light")]
 	Light = 1,
+
+	[Description("Dark")]
 	Dark = 2,
 }
